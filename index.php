1acf6fb38052b15e16a22d35cf4e3bb16aa94c4f
yukari_n <yukari_n@outlook.jp>
2016-12-10 14:41:46 +0900

    edit

--- index.php
+++ index.php
@@ -2,12 +2,13 @@
 <html>
 <head>
 <meta charset="UTF-8" />
-'<meta name="robots" content="noindex" />
+<meta name="robots" content="noindex" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <meta http-equiv="imagetoolbar" content="no" />
 <meta name="viewport" content="width=device-width,initial-scale=1" />
 <meta name="format-detection" content="telephone=no,address=no,email=no" />
-<title></title>
+<title>under construction</title>
+<link href="https://fonts.googleapis.com/earlyaccess/sawarabigothic.css" rel="stylesheet" />
 </head>
 <body><?php
 echo 'under construction';
@@ -27,7 +28,9 @@ $map[4][5] = BLACK;
 $map[4][4] = WHITE;
 $map[5][5] = WHITE;
 
+echo '――――――――<br />';
 for($i=1;$i<=8;++$i){
+	echo '|';
 	for($j=1;$j<=8;++$j){
 		if(!isset($map[$i][$j])){
 			echo '　';
@@ -35,8 +38,10 @@ for($i=1;$i<=8;++$i){
 		else{
 			echo $map[$i][$j];
 		}
+		echo '|';
 	}
-	echo '<br />';
+	echo '|<br />';
+	echo '――――――――<br />';
 }
 ?></body>
 </html>
