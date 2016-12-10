63e4bcc029fc1c83be0c30803d8af073385ddea5
yukari_n <yukari_n@outlook.jp>
2016-12-10 20:08:54 +0900

    exclude out of the area

--- search.php
+++ search.php
@@ -5,8 +5,10 @@ function search_can_put($map,$putmap){
 		$stone = str_split($coord);
 		for($i=-1;$i<=1;++$i){ //横方向
 			$x = $stone[0] + $i;
+			if($x < 1 || $x > 8){continue;} //枠外
 			for($j=-1;$j<=1;++$j){ //縦方向
 				$y = $stone[1] + $j;
+				if($y < 1 || $y > 8){continue;} //枠外
 				if(isset($_SESSION['map'][$x][$y]) || ($i == 0 && $j == 0)){
 					continue;
 				}
