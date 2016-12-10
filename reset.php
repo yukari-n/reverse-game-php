413fd545732100c7d9faf9d67de9c78e748fd8b7
yukari_n <yukari_n@outlook.jp>
2016-12-10 15:39:56 +0900

    edit

--- reset.php
+++ reset.php
@@ -2,6 +2,14 @@
 //上手く動いてない
 print_r($_SESSION);
 $_SESSION = array();
+
+if(isset($_COOKIE['count'])){
+	setcookie('count', '', time() - 1800, '/');
+}
+if(isset($_COOKIE['putted'])){
+	setcookie('putted', '', time() - 1800, '/');
+}
+
 session_destroy();
 
 echo '<p>Data was deleted.</p>';
