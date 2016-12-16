95873ee0437cdfd1237bae1bbf856850c3afd796
yukari_n <yukari_n@outlook.jp>
2016-12-16 21:49:30 +0900

    文言の変更

--- reset.php
+++ reset.php
@@ -1,7 +1,21 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8" />
+<meta name="robots" content="noindex" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta http-equiv="imagetoolbar" content="no" />
+<meta name="viewport" content="width=device-width,initial-scale=1" />
+<meta name="format-detection" content="telephone=no,address=no,email=no" />
+<title>はさんでひっくりかえすやつ</title>
+<link href="https://fonts.googleapis.com/earlyaccess/sawarabigothic.css" rel="stylesheet" />
+<link href="style.css" rel="stylesheet" />
+</head>
+<body>
+<h1>はさんでひっくりかえすやつ</h1><?php
 session_start();
 
-print_r($_SESSION);
+/print_r($_SESSION);
 $_SESSION = array();
 
 if(isset($_COOKIE['count'])){
@@ -13,8 +27,10 @@ if(isset($_COOKIE['put'])){
 
 session_destroy();
 
-echo '<p>Data was deleted.</p>';
+echo '<p>(・ω・)りせっとしました</p>';
 
-print_r($_SESSION);
+//print_r($_SESSION);
 
-echo '<p><a href="/">Back</a></p>';
+echo '<p><a href="/">戻る</a></p>';
+?></body>
+</html>
