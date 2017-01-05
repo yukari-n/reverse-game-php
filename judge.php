6b6489d756bb13681ee9fe5162d8b70f290cbbf9
yukari_n <yukari_n@outlook.jp>
2017-01-06 01:19:39 +0900

    台詞の日本語化

--- judge.php
+++ judge.php
@@ -1,34 +1,12 @@
 <?php
 echo '<p>(・ω・)',count($_SESSION['cp_map']),'個の白石と',count($_SESSION['pl_map']),'の黒石があるね</p>';
-//数がおかしい（少ない）
-
-echo '<p>I could put ',count($_SESSION['cp_can_put']),' places.</p>';
-//print_r($_SESSION['cp_can_put']);
-
-//$black_count = count($_SESSION['pl_map']);
-//$white_count = count($_SESSION['cp_map']);
-$black_count = 0;
-$white_count = 0;
-for($i=-1;$i<2;++$i){ //横方向
-	for($j=-1;$j<2;++$j){ //縦方向
-		if($_SESSION['map'][$i][$j] == BLACK){
-			++$black_count;
-		}
-		elseif($_SESSION['map'][$i][$j] == WHITE){
-			++$black_count;
-		}
-	}
-}
-
-echo '<p>There are ',$white_count,' white stones.</p>';
-echo '<p>There are ',$black_count,' black stones.</p>';
 
 if($white_count > $black_count){
-	echo '<p style="color:#00f;">I win!</p>';
+	echo '<p style="color:#00f;">(・∀・)ぼくの勝ち！</p>';
 }
 elseif($white_count < $black_count){
-	echo '<p style="color:#f00;">You win!</p>';
+	echo '<p style="color:#f00;">(´・ω・｀)きみの勝ち！</p>';
 }
 else{
-	echo '<p style="color:#f0f;">Draw!</p>';
+	echo '<p style="color:#f0f;">(・ω・)引き分け！</p>';
 }
