a55e4d082caf92ee35fa2f9e008d45e8441c6d9b
yukari_n <yukari_n@outlook.jp>
2017-01-06 00:08:26 +0900

    ファイルを分割しただけ（index.phpはできるだけHTMLのみにする）

--- index.php
+++ index.php
@@ -172,41 +172,7 @@ echo $_SESSION['count'].'手目';
 echo '<form action="index.php" method="post">';
 
 if($_SESSION['count'] > 0 && count($_SESSION['cp_can_put']) == 0 && count($_SESSION['pl_can_put']) == 0 || count($_SESSION['cp_map']) == 0 || count($_SESSION['pl_map']) == 0){
-
-	echo '<p>There are ',count($_SESSION['cp_map']),' white stones.</p>';
-	echo '<p>There are ',count($_SESSION['pl_map']),' black stones.</p>';
-	//数がおかしい（少ない）
-
-	echo '<p>I could put ',count($_SESSION['cp_can_put']),' places.</p>';
-	//print_r($_SESSION['cp_can_put']);
-
-	//$black_count = count($_SESSION['pl_map']);
-	//$white_count = count($_SESSION['cp_map']);
-	$black_count = 0;
-	$white_count = 0;
-	for($i=-1;$i<2;++$i){ //横方向
-		for($j=-1;$j<2;++$j){ //縦方向
-			if($_SESSION['map'][$i][$j] == BLACK){
-				++$black_count;
-			}
-			elseif($_SESSION['map'][$i][$j] == WHITE){
-				++$black_count;
-			}
-		}
-	}
-
-	echo '<p>There are ',$white_count,' white stones.</p>';
-	echo '<p>There are ',$black_count,' black stones.</p>';
-
-	if($white_count > $black_count){
-		echo '<p style="color:#00f;">I win!</p>';
-	}
-	elseif($white_count < $black_count){
-		echo '<p style="color:#f00;">You win!</p>';
-	}
-	else{
-		echo '<p style="color:#f0f;">Draw!</p>';
-	}
+	include('judge.php');
 }
 else{
 	echo '<input type="submit" value="OK"> <input type="radio" id="pass" name="put" value="pass"><label for="pass" class="selected pass">&#x25a0; パス</label>';
---
+++ judge.php
@@ -0,0 +1,35 @@
+<?php
+echo '<p>There are ',count($_SESSION['cp_map']),' white stones.</p>';
+echo '<p>There are ',count($_SESSION['pl_map']),' black stones.</p>';
+//数がおかしい（少ない）
+
+echo '<p>I could put ',count($_SESSION['cp_can_put']),' places.</p>';
+//print_r($_SESSION['cp_can_put']);
+
+//$black_count = count($_SESSION['pl_map']);
+//$white_count = count($_SESSION['cp_map']);
+$black_count = 0;
+$white_count = 0;
+for($i=-1;$i<2;++$i){ //横方向
+	for($j=-1;$j<2;++$j){ //縦方向
+		if($_SESSION['map'][$i][$j] == BLACK){
+			++$black_count;
+		}
+		elseif($_SESSION['map'][$i][$j] == WHITE){
+			++$black_count;
+		}
+	}
+}
+
+echo '<p>There are ',$white_count,' white stones.</p>';
+echo '<p>There are ',$black_count,' black stones.</p>';
+
+if($white_count > $black_count){
+	echo '<p style="color:#00f;">I win!</p>';
+}
+elseif($white_count < $black_count){
+	echo '<p style="color:#f00;">You win!</p>';
+}
+else{
+	echo '<p style="color:#f0f;">Draw!</p>';
+}
